7116a77f707f1796ab9f2442e875764039d7d936
200602233 <200602233@student.georgianc.on.ca>
2026-06-08 16:44:12 -0400

    added btn binding and a function to switch frag view in the constraint layout

--- app/src/main/java/com/example/weatherpal/MainActivity.java
+++ app/src/main/java/com/example/weatherpal/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.weatherpal;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import java.util.zip.Inflater;
 import com.example.weatherpal.databinding.ActivityMainBinding;
@@ -31,6 +35,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // search btn
+        binding.searchBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                switchFragment(new SearchFragment());
+            }
+        });
 
+        // saved btn
+        binding.savedBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                switchFragment(new SearchFragment());
+            }
+        });
+
+        // settings btn
+        binding.settingsBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                switchFragment(new SearchFragment());
+            }
+        });
+    }
+
+    // Frag function to show frag view in contraint layout
+    private void switchFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragArea, fragment);
+        ft.commit();
     }
 }
