928ad9ab07dd6d20ff90053c02cbc1b059c8eb7c
200602233 <200602233@student.georgianc.on.ca>
2026-06-11 14:23:47 -0400

    sucessfully added onCLick events for each container in our app to allow user to interact with them :)

--- app/src/main/java/com/example/weatherpal/SettingsFragment.java
+++ app/src/main/java/com/example/weatherpal/SettingsFragment.java
@@ -1,5 +1,8 @@
 package com.example.weatherpal;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,34 +10,42 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.weatherpal.databinding.ActivityMainBinding;
+import com.example.weatherpal.databinding.FragmentSettingsBinding;
 
 
 public class SettingsFragment extends Fragment {
 
+    private FragmentSettingsBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        //return inflater.inflate(R.layout.fragment_settings, container, false);
 
-
+        // binding
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
 
         // code from week 4 Intent Slide 14
-        //� Use try/catch as an alternative to resolveActivity() on Android 11+ (API 30+).
-//        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-//        emailIntent.setData(Uri.parse("mailto:"));
-//        emailIntent.putExtra(Intent.EXTRA_EMAIL,
-//                new String[]{"support@weathernow.app"});
-//        emailIntent.putExtra(Intent.EXTRA_SUBJECT,
-//                "WeatherNow — Feedback");
-//// Always check a handler exists before calling startActivity
-//        if (emailIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivity(emailIntent);
-//        } else {
-//            Toast.makeText(this,
-//                    "No email app found",
-//                    Toast.LENGTH_SHORT).show();
-//        }
+        binding.feedback.setOnClickListener(v -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:"));
+            emailIntent.putExtra(Intent.EXTRA_EMAIL,
+                    new String[]{"help@weatherpal.ca"});
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT,
+                    "WeatherPal — Feedback Suggestion");
+            // teachers suggestion : Use try/catch as an alternative to resolveActivity() on Android 11+ (API 30+).
+            try{
+                startActivity(emailIntent);
+            } catch (ActivityNotFoundException e){
+                Toast.makeText(requireContext(),
+                        "No email app found :(",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         // code from week 4-Intent slide 15
         /*
@@ -42,41 +53,52 @@ public class SettingsFragment extends Fragment {
 Link to your own GitHub repo. The URL can be anything
 valid — the marker checks the intent fires.
          */
-        // Open a URL in the default browser
-//        String url = "https://github.com/yourusername/weathernow";
-//        Intent browserIntent = new Intent(
-//                Intent.ACTION_VIEW,
-//                Uri.parse(url)
-//        );
-//// Guard against no browser being installed
-//        try {
-//            startActivity(browserIntent);
-//        } catch (ActivityNotFoundException e) {
-//            Toast.makeText(
-//                    this,
-//                    "No browser app found",
-//                    Toast.LENGTH_SHORT
-//            ).show();
-//        }
-
+        // github intent
+        binding.github.setOnClickListener(v ->{
+            String url = "https://github.com/200602233/WeatherPal";
+            Intent browserIntent = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(url)
+            );
+            // teacher note::::
+        // Guard against no browser being installed
+            try {
+                startActivity(browserIntent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(
+                        requireContext(),
+                        "No browser app found :(",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
 
         // code from week 4-Intent slide 16
-        // Build a sharing Intent for plain text
-//        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-//        shareIntent.setType("text/plain"); // MIME type
-//        shareIntent.putExtra(
-//                Intent.EXTRA_TEXT,
-//                "Check out WeatherNow! 🌤 https://example.com"
-//        );
-//// Wrap in a chooser — user picks the app
-//        Intent chooser = Intent.createChooser(
-//                shareIntent,
-//                "Share WeatherNow via..."
-//        );
-        //// Guard: show Toast if nothing handles ACTION_SEND
-//        if (shareIntent.resolveActivity(
-//                getPackageManager()) != null) {
-//            startActivity(chooser);
-//        }
+        // share app intent
+        binding.shareApp.setOnClickListener(v ->{
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain"); // MIME type (teacher note)
+            shareIntent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    "🌤 WeatherPal, the most accurate weather app - https://google.com"
+            );
+        // user picks the app to share to
+            Intent chooser = Intent.createChooser(
+                    shareIntent,
+                    "Share WeatherPal ..."
+            );
+            // teachers note:
+            // Guard: show Toast if nothing handles ACTION_SEND
+            // added requireActvivty so it would properly call getPackageManager()
+            if (shareIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+                startActivity(chooser);
+            } else{
+                Toast.makeText(requireContext(),
+                        "Sharing errored :(",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return binding.getRoot();
     }
 }
