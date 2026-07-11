3b25948713a0838126a54c307e65b5e93d2715f0
200602233 <200602233@student.georgianc.on.ca>
2026-07-11 18:47:50 -0400

    copied over week 9's api code to work off of (i believe its just a post/put rewuest not get)

--- app/src/main/java/com/example/weatherpal/repository/WeatherRepository.java
+++ app/src/main/java/com/example/weatherpal/repository/WeatherRepository.java
@@ -1,4 +1,19 @@
 package com.example.weatherpal.repository;
 
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class WeatherRepository {
+    // week 9 api code
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public static void get(String url, Callback callback) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
 }
