e6ce394f72ab88974538dfcc5147b065f7ff00ff
200602233 <200602233@student.georgianc.on.ca>
2026-07-12 17:15:01 -0400

    manually added api url using week 9 setup for api

--- app/src/main/java/com/example/weatherpal/repository/WeatherRepository.java
+++ app/src/main/java/com/example/weatherpal/repository/WeatherRepository.java
@@ -9,11 +9,21 @@ import okhttp3.Request;
 public class WeatherRepository {
     // week 9 api code
     private static final OkHttpClient client = new OkHttpClient();
-    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
-    public static void get(String url, Callback callback) {
+//    private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    public static void get(String city, Callback callback) {
+        //manual http link
+        // The endpoint is: GET https://api.weatherapi.com/v1/current.json?key={key}&q={city}&aqi=no
+        HttpUrl url = HttpUrl.parse("https://api.weatherapi.com/v1/current.json")
+                .newBuilder()
+                .addQueryParameter("key", "4b1c28ddf81a49d5a6c155408261107")
+                .addQueryParameter("q", city)
+                .addQueryParameter("api", "no")
+                .build();
+        // request
         Request request = new Request.Builder()
                 .url(url)
                 .build();
+        // cleint call
         client.newCall(request).enqueue(callback);
     }
 }
