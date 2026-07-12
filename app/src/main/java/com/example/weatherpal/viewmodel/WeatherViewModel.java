8999ea375b18979fc48b2b3245b0b2fe4eb3dcb6
200602233 <200602233@student.georgianc.on.ca>
2026-07-12 18:04:07 -0400

    followed week 9 and connected model and viewmodel. should properly call the location, current, name, and temp

--- app/src/main/java/com/example/weatherpal/viewmodel/WeatherViewModel.java
+++ app/src/main/java/com/example/weatherpal/viewmodel/WeatherViewModel.java
@@ -1,5 +1,63 @@
 package com.example.weatherpal.viewmodel;
 
-public class WeatherViewModel {
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.weatherpal.model.WeatherModel;
+import com.example.weatherpal.repository.WeatherRepository;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+public class WeatherViewModel extends ViewModel {
+    // followed week 9 api code
+    WeatherModel weatherModel = new WeatherModel();
+
+    private final MutableLiveData<WeatherModel> weatherData = new MutableLiveData<WeatherModel>();
+    public LiveData<WeatherModel> getWeatherData() {
+        return weatherData;
+    }
+
+    // refresh
+    public void Refresh(String city){
+        WeatherRepository.get(city, new okhttp3.Callback(){
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                String responseData = response.body().string();
+                Log.i("tag", responseData); // not sure if needed
+                JSONObject json = null;
+                try{
+                    json = new JSONObject(responseData);
+
+                    JSONObject location = json.getJSONObject("location");
+                    JSONObject current = json.getJSONObject("current");
+
+                    String strCity = location.getString("name");
+                    String strTempC = current.getString("temp_c") + "°C";
+
+                    weatherModel.setCity(strCity);
+                    weatherModel.setTempC(strTempC);
+
+                    weatherData.postValue(weatherModel);
+
+                } catch (Exception e){
+                    Log.e("tag", "Error while parsing JSON: ", e);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.i("tag", "onFailure"); // not sure if needed
+            }
+        });
+    }
 }
