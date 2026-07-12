aec213f41c809cda18db4f6b64dbbc08b4d6bda6
200602233 <200602233@student.georgianc.on.ca>
2026-07-12 18:32:19 -0400

    changed binding and followed week 9 layout to have data overwrite depending on which card is clicked

--- app/src/main/java/com/example/weatherpal/view/WeatherDetailActivity.java
+++ app/src/main/java/com/example/weatherpal/view/WeatherDetailActivity.java
@@ -8,13 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.weatherpal.R;
 import com.example.weatherpal.databinding.ActivityWeatherDetailBinding;
+import com.example.weatherpal.viewmodel.WeatherViewModel;
 
 public class WeatherDetailActivity extends AppCompatActivity {
 
     private ActivityWeatherDetailBinding binding;
+    private WeatherViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +35,27 @@ public class WeatherDetailActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         String city = getIntent().getStringExtra("city");
-        setToolBar(city);
-        setWeatherDetails(city);
+//        setToolBar(city);
+//        setWeatherDetails(city);
+
+        // api connection to fill out data (commented out past functions)
+        viewModel = new ViewModelProvider(this).get(WeatherViewModel.class);
+
+        viewModel.getWeatherData().observe(this, weatherData -> {
+            // toolbar binding
+            binding.toolbar.setTitle(weatherData.getCity());
+            binding.toolbar.setTitle(weatherData.getRegion());
+
+            // cards
+            binding.weatherCondition.setText(weatherData.getWeatherCondition());
+            binding.tempC.setText(weatherData.getTempC());
+            binding.tempF.setText(weatherData.getTempF());
+            binding.humidity.setText(weatherData.getHumidity());
+            binding.wind.setText(weatherData.getWind());
+            binding.feelsLike.setText(weatherData.getFeelsLike());
+            binding.uvIndex.setText(weatherData.getUvIndex());
+        });
+
 
         //back btn
         binding.backBtn.setOnClickListener(view -> backBtnAction());
@@ -44,79 +67,79 @@ public class WeatherDetailActivity extends AppCompatActivity {
         startActivity(intent);
     }
     // function to change header/toolbar title to display selected city
-    private void setToolBar(String city){
-        if(city.equals("London")){
-            binding.toolbar.setTitle("London");
-            binding.toolbar.setSubtitle("City of London, Greater London");
-        }
-        else if(city.equals("Toronto")){
-            binding.toolbar.setTitle("Toronto");
-            binding.toolbar.setSubtitle("Ontario");
-        }
-        else if(city.equals("Tokyo")){
-            binding.toolbar.setTitle("Tokyo");
-            binding.toolbar.setSubtitle("Tokyo-to");
-        }
-        else if(city.equals("Sydney")){
-            binding.toolbar.setTitle("Sydney");
-            binding.toolbar.setSubtitle("New South Wales");
-        }
-        else if(city.equals("New York")){
-            binding.toolbar.setTitle("New York");
-            binding.toolbar.setSubtitle("New York State");
-        }
-    }
-
-    // function to display weather details for the selected city
-    // for now we're using hardcoded values
-    private void setWeatherDetails(String city){
-        String celsius = "℃";
-        String fahrenheit = "℉";
-
-        if(city.equals("London")){
-            binding.weatherCondition.setText("Cloudy");
-            binding.tempC.setText("14" + celsius);
-            binding.tempF.setText("57" + fahrenheit);
-            binding.humidity.setText("78%");
-            binding.wind.setText("18 kph");
-            binding.feelsLike.setText("12" + celsius);
-            binding.uvIndex.setText("2 - Low");
-        }
-        else if(city.equals("Toronto")){
-            binding.weatherCondition.setText("Sunny");
-            binding.tempC.setText("22" + celsius);
-            binding.tempF.setText("72" + fahrenheit);
-            binding.humidity.setText("83%");
-            binding.wind.setText("16 kph");
-            binding.feelsLike.setText("26" + celsius);
-            binding.uvIndex.setText("8 - Very High");
-        }
-        else if(city.equals("Tokyo")){
-            binding.weatherCondition.setText("Clear");
-            binding.tempC.setText("28" + celsius);
-            binding.tempF.setText("82" + fahrenheit);
-            binding.humidity.setText("64%");
-            binding.wind.setText("10 kph");
-            binding.feelsLike.setText("32" + celsius);
-            binding.uvIndex.setText("7 - High");
-        }
-        else if(city.equals("Sydney")){
-            binding.weatherCondition.setText("Sunny");
-            binding.tempC.setText("20" + celsius);
-            binding.tempF.setText("68" + fahrenheit);
-            binding.humidity.setText("56%");
-            binding.wind.setText("15 kph");
-            binding.feelsLike.setText("25" + celsius);
-            binding.uvIndex.setText("5 - Moderate");
-        }
-        else if(city.equals("New York")){
-            binding.weatherCondition.setText("Rainy");
-            binding.tempC.setText("11" + celsius);
-            binding.tempF.setText("52" + fahrenheit);
-            binding.humidity.setText("63%");
-            binding.wind.setText("9 kph");
-            binding.feelsLike.setText("15" + celsius);
-            binding.uvIndex.setText("6 - High");
-        }
-    }
+//    private void setToolBar(String city){
+//        if(city.equals("London")){
+//            binding.toolbar.setTitle("London");
+//            binding.toolbar.setSubtitle("City of London, Greater London");
+//        }
+//        else if(city.equals("Toronto")){
+//            binding.toolbar.setTitle("Toronto");
+//            binding.toolbar.setSubtitle("Ontario");
+//        }
+//        else if(city.equals("Tokyo")){
+//            binding.toolbar.setTitle("Tokyo");
+//            binding.toolbar.setSubtitle("Tokyo-to");
+//        }
+//        else if(city.equals("Sydney")){
+//            binding.toolbar.setTitle("Sydney");
+//            binding.toolbar.setSubtitle("New South Wales");
+//        }
+//        else if(city.equals("New York")){
+//            binding.toolbar.setTitle("New York");
+//            binding.toolbar.setSubtitle("New York State");
+//        }
+//    }
+//
+//    // function to display weather details for the selected city
+//    // for now we're using hardcoded values
+//    private void setWeatherDetails(String city){
+//        String celsius = "℃";
+//        String fahrenheit = "℉";
+//
+//        if(city.equals("London")){
+//            binding.weatherCondition.setText("Cloudy");
+//            binding.tempC.setText("14" + celsius);
+//            binding.tempF.setText("57" + fahrenheit);
+//            binding.humidity.setText("78%");
+//            binding.wind.setText("18 kph");
+//            binding.feelsLike.setText("12" + celsius);
+//            binding.uvIndex.setText("2 - Low");
+//        }
+//        else if(city.equals("Toronto")){
+//            binding.weatherCondition.setText("Sunny");
+//            binding.tempC.setText("22" + celsius);
+//            binding.tempF.setText("72" + fahrenheit);
+//            binding.humidity.setText("83%");
+//            binding.wind.setText("16 kph");
+//            binding.feelsLike.setText("26" + celsius);
+//            binding.uvIndex.setText("8 - Very High");
+//        }
+//        else if(city.equals("Tokyo")){
+//            binding.weatherCondition.setText("Clear");
+//            binding.tempC.setText("28" + celsius);
+//            binding.tempF.setText("82" + fahrenheit);
+//            binding.humidity.setText("64%");
+//            binding.wind.setText("10 kph");
+//            binding.feelsLike.setText("32" + celsius);
+//            binding.uvIndex.setText("7 - High");
+//        }
+//        else if(city.equals("Sydney")){
+//            binding.weatherCondition.setText("Sunny");
+//            binding.tempC.setText("20" + celsius);
+//            binding.tempF.setText("68" + fahrenheit);
+//            binding.humidity.setText("56%");
+//            binding.wind.setText("15 kph");
+//            binding.feelsLike.setText("25" + celsius);
+//            binding.uvIndex.setText("5 - Moderate");
+//        }
+//        else if(city.equals("New York")){
+//            binding.weatherCondition.setText("Rainy");
+//            binding.tempC.setText("11" + celsius);
+//            binding.tempF.setText("52" + fahrenheit);
+//            binding.humidity.setText("63%");
+//            binding.wind.setText("9 kph");
+//            binding.feelsLike.setText("15" + celsius);
+//            binding.uvIndex.setText("6 - High");
+//        }
+//    }
 }
