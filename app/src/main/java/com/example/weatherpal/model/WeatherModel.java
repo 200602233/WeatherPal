c77a42599c8f311e44ab7f481dd934f49a6e4ad2
200602233 <200602233@student.georgianc.on.ca>
2026-07-12 17:35:01 -0400

    added variables and getters & setters in the WeatherModel

--- app/src/main/java/com/example/weatherpal/model/WeatherModel.java
+++ app/src/main/java/com/example/weatherpal/model/WeatherModel.java
@@ -1,4 +1,79 @@
 package com.example.weatherpal.model;
 
 public class WeatherModel {
+    // variables that we have in the WeatherDetails atcivity and xml
+    private String city;
+    private String weatherCondition;
+    private String tempC;
+    private String tempF;
+    private String humidity;
+    private String wind;
+    private String feelsLike;
+    private String uvIndex;
+
+    // get and sets (used generate > Getters and Setters)
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getWeatherCondition() {
+        return weatherCondition;
+    }
+
+    public void setWeatherCondition(String weatherCondition) {
+        this.weatherCondition = weatherCondition;
+    }
+
+    public String getTempC() {
+        return tempC;
+    }
+
+    public void setTempC(String tempC) {
+        this.tempC = tempC;
+    }
+
+    public String getTempF() {
+        return tempF;
+    }
+
+    public void setTempF(String tempF) {
+        this.tempF = tempF;
+    }
+
+    public String getWind() {
+        return wind;
+    }
+
+    public void setWind(String wind) {
+        this.wind = wind;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(String feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public String getUvIndex() {
+        return uvIndex;
+    }
+
+    public void setUvIndex(String uvIndex) {
+        this.uvIndex = uvIndex;
+    }
 }
