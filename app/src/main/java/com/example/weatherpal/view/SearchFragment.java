d8b57c1eb0ee979d008ed28386d21963b6a6577c
200602233 <200602233@student.georgianc.on.ca>
2026-08-07 11:27:54 -0400

    - added necessary imports - added the private for Handler as we need it for a delay with search - added Runnable var below it
    Search display:
    - used our WeatherDetailsActivity for reference
    - took fun getDynamicSearchResults from the LiveData in WeatherViewMOdel and made it observe it for any changes.
    - when it hears changes, it clears the list, then adds all the new search resulst to the RecyclerView
    - validation:
       - if results searched return 0, the hidden empty message appears on the screen below the search (will update what shows later)
    
    Search with Delay:
    - used our SplashActivity, and a few web links (souced in commented code) as refernece
    - used binding on the searchBar to add a TextChangedListener
       - onTextChanged(){}
         - handler method here with new Runnable
         - void run
          - if the search.length is 3 or more characters, the search actually starts (char num may change)
          - added 500 for the delayMillis

--- app/src/main/java/com/example/weatherpal/view/SearchFragment.java
+++ app/src/main/java/com/example/weatherpal/view/SearchFragment.java
@@ -7,10 +7,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.weatherpal.R;
 import com.example.weatherpal.databinding.FragmentSearchBinding;
@@ -19,6 +23,8 @@ import com.example.weatherpal.viewmodel.WeatherViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SearchFragment extends Fragment implements ItemClickListener{
 
@@ -27,6 +33,10 @@ public class SearchFragment extends Fragment implements ItemClickListener{
     List<WeatherModel> weatherList;
     // adapater for onclick
     MyAdapter myAdapter;
+
+    // assign 3
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable runnable;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,14 +48,14 @@ public class SearchFragment extends Fragment implements ItemClickListener{
 
         // data source Assign 2 addition
         weatherList = new ArrayList<>();
-        WeatherModel place1 = new WeatherModel(R.drawable.rain_icon, "Toronto", "Ontario, Canada", 43.6667, -79.4167);
-        WeatherModel place2 = new WeatherModel(R.drawable.rain_icon, "Montreal", "Québec, Canada", 45.5, -73.5833);
-        WeatherModel place3 = new WeatherModel(R.drawable.rain_icon, "Winnipeg", "Manitoba, Canada", 49.8833, -97.1667);
-
-        // add to list
-        weatherList.add(place1);
-        weatherList.add(place2);
-        weatherList.add(place3);
+//        WeatherModel place1 = new WeatherModel(R.drawable.rain_icon, "Toronto", "Ontario, Canada", 43.6667, -79.4167);
+//        WeatherModel place2 = new WeatherModel(R.drawable.rain_icon, "Montreal", "Québec, Canada", 45.5, -73.5833);
+//        WeatherModel place3 = new WeatherModel(R.drawable.rain_icon, "Winnipeg", "Manitoba, Canada", 49.8833, -97.1667);
+//
+//        // add to list
+//        weatherList.add(place1);
+//        weatherList.add(place2);
+//        weatherList.add(place3);
 
 
         // Assign 1 binding
@@ -70,6 +80,52 @@ public class SearchFragment extends Fragment implements ItemClickListener{
 
         myAdapter = new MyAdapter(weatherList);
         binding.recyclerView.setAdapter(myAdapter);
+
+        // search
+        // same get() layout as the one in WeatherDetailActivity for getWeatherDetail() from
+        // getLiveData in ViewModel
+        viewModel.getDynamicSearchResults().observe(getViewLifecycleOwner(), results -> {
+            // clears list
+            weatherList.clear();
+            // shows all results that were searched
+            weatherList.addAll(results);
+            // tells recyclerview that list has been changed and refreshes
+            myAdapter.notifyDataSetChanged();
+
+            // if zero results show emptyMessage
+            if(results.size() == 0){
+                binding.emptyMessage.setVisibility(View.VISIBLE);
+            } else{
+                binding.emptyMessage.setVisibility(View.GONE);
+            }
+        });
+
+        //search + delay (MIN 300 ms)
+        // web-links i read about the addTextChangedListenr
+        // https://www.geeksforgeeks.org/android/ontextchangedlistener-in-android/
+        // https://developer.android.com/reference/android/text/TextWatcher
+        binding.searchBar.addTextChangedListener(new TextWatcher() {
+            // leave empty unless we need or want code but dont think need
+            @Override
+            public void afterTextChanged(Editable s) {}
+            // leave empty unless we need or want code but dont think need
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // took from splash and re-coded to fit
+                new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run(){
+                        // searches after 3 characters (idk thought cool but could chnage)
+                        if (s.length() >= 3){
+                            viewModel.dynamicSearch(s.toString());
+                        }
+                    }
+                }, 500); // wait 500 ms
+            }
+        });
 
         myAdapter.setClickListener(this);
         // return
